a56adefab5944819d88b9af663d5f57840c431ad
Gutsey68 <139963620+Gutsey68@users.noreply.github.com>
2024-10-22 14:45:17 +0200

    fix: responsiv bug

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import PasswordCard from './components/PasswordCard';
 
 function App() {
     return (
-        <main className="flex min-h-screen w-full items-center justify-center bg-dark font-jetbrain">
-            <div className="">
+        <main className="flex min-h-screen w-full flex-col items-center justify-center bg-dark font-jetbrain">
+            <div className="w-full max-sm:px-4">
                 <h1 className="pb-8 text-center text-xl text-gray">Password Generator</h1>
                 <PasswordCard />
                 <BodyCard />
